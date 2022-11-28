60be6881a81cd7b7cc3b639201021b087360de7e
Azivus <pankeev2004@gmail.com>
2022-11-28 21:14:14 +0100

    Commit message

---
+++ dist/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['lab5_0',['lab5',['../main_8c.html#a9203ce2709f1f3965903d89d863b88d8',1,'main.c']]],
+  ['lab6_1',['lab6',['../main_8c.html#a307bb8d56a3e56ebce966a55fd5be2b2',1,'main.c']]]
+];
